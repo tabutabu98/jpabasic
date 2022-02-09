2a2c941485ac192a996ab6aaac239bac5716b095
syc <songyechan.kr@gmail.com>
2022-02-09 23:41:39 +0900

    feat Hello JPA - 프로젝트 생성, 애플리케이션 개발 @syc

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,53 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member1 = new Member();
+//            member1.setId(1L);
+//            member1.setName("syc");
+//            em.persist(member1);
+//            Member member2 = new Member();
+//            member2.setId(2L);
+//            member2.setName("홍길동");
+//            em.persist(member2);
+
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//            findMember.setName("송예찬");
+
+            // Member 객체가 대상
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
