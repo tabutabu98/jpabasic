da3fb541ebefa97be4a17a8c007a3ac354d25a6b
syc <syc@partdb.com>
2022-03-10 21:08:44 +0900

    feat 객체지향 쿼리 언어1 - 기본 문법 소개 @syc

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,9 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -16,49 +19,61 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 값 타입 컬렉션
+            // 객체지향 쿼리 언어1 - 기본 문법 소개
+
+//            // JPQL
+//            List<Member> result = em.createQuery(
+//                    "select m from Member m where m.username like '%kim%'",
+//                    Member.class
+//            ).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
+//            }
+
+            // Criteria
+//            // Criteria 사용 준비
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+//
+//            // 루트 클래스 (조회를 시작할 클래스)
+//            Root<Member> m = query.from(Member.class);
+//
+//            // 쿼리 생성
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+//            List<Member> resultList = em.createQuery(cq).getResultList();
+
+            // Native SQL
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            em.persist(member);
+//
+//            // flush -> commit, query
+//
+//            // commit 시점에서도 쿼리가 나가지만 엔티티매니저의 쿼리 메서드에서도 flush가 된다.
+//            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, CITY, STREET, ZIPCODE, USERNAME from MEMBER", Member.class)
+//                    .getResultList();
+//
+//            for (Member member1 : resultList) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+            // JDBC 직접 사용, SpringJdbcTemplate
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
             em.persist(member);
 
+            // flush -> commit, query
+
+            // flush를 해주는 이유는 JDBC나 SpringJdbcTemplate같은 경우 JPA와 관련이 없기 때문에 영속성 컨테스트에 있는 데이터를 디비에 뿌려주는 기능이 없기 때문
             em.flush();
-            em.clear();
 
-            System.out.println("==============START==============");
-            Member findMember = em.find(Member.class, member.getId());
+            // 결과 0
+            // dbconn.executeQuery("select * from member");
 
-//            List<Address> addressHistory = findMember.getAddressHistory();
-//            for (Address address : addressHistory) {
-//                System.out.println("address = " + address.getCity());
-//            }
-//
-//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-//            for (String favoriteFood : favoriteFoods) {
-//                System.out.println("favoriteFood = " + favoriteFood);
-//            }
-
-            // homeCity -> newCity, 이는 잘못된 방법
-//            findMember.getHomeAddress().setCity("newCity");
-
-            // 새로운 인스턴스를 통으로 갈아끼워야한다.
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
